a34a950f4e1d628686202e605c6b5e3e5d30d92a
angelettecortez <cortezangelette@gmail.com>
2024-11-04 18:31:28 +0800

    Create classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Object Oriented Programming"
+let classSchedule = "4:00-5:00 in Monday and Friday"
+let classroom = "Computer Laboratory C"
+let classInstructor = "Mr. Ryan Jay Cuevas"
+let studentName = "Angelette H. Cortez"
+
+console.log(${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.)
